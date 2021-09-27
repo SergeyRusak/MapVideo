31ddff5ab405ba9add1076aae9a5ec11b96457d0
SergeyRusak <rusak200281@gmail.com>
2021-09-27 20:57:25 +0800

    tried to create MapRender

--- src/com/animatik/mapvideo/Main.java
+++ src/com/animatik/mapvideo/Main.java
@@ -1,5 +1,7 @@
 package com.animatik.mapvideo;
 
+import com.animatik.mapvideo.listeners.InGameListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -11,6 +13,9 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         super.onEnable();
+        Bukkit.getServer().broadcastMessage("IT WORKS!!!");
+        getServer().getPluginManager().registerEvents(new InGameListener(), this);
     }
 }
---
+++ src/com/animatik/mapvideo/listeners/InGameListener.java
@@ -0,0 +1,40 @@
+package com.animatik.mapvideo.listeners;
+
+
+import com.animatik.mapvideo.maprenders.PlainMapRender;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.map.MapView;
+
+public class InGameListener implements Listener {
+
+
+    @EventHandler
+    public void onPlayerTouch2(PlayerInteractEntityEvent e) {
+        if (e.getRightClicked().getType().equals(EntityType.ITEM_FRAME)){
+            e.getPlayer().sendRawMessage("Ты кликнул по рамке");
+            ItemFrame fr = (ItemFrame) e.getRightClicked();
+            MapView mv = Bukkit.getServer().createMap(e.getPlayer().getWorld());
+            mv.getRenderers().clear();
+            mv.getRenderers().add(new PlainMapRender());
+            ItemStack itemmap = new ItemStack(Material.FILLED_MAP);
+            MapMeta mm = (MapMeta) itemmap.getItemMeta();
+            mm.setMapView(mv);
+            itemmap.setItemMeta(mm);
+            fr.setItem(itemmap);
+    }
+
+}
+}
+
+
+
+
+
